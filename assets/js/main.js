627891c41e868250005ca27f5b3e8bc498cee286
ssomeshg <121214623+ssomeshg@users.noreply.github.com>
2025-01-28 16:01:37 +0530

    navbar functionalities and responsive changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,181 +1,267 @@
 "use strict";
 
+// Menu Bar
+const menuBar = document.getElementById("menu-bar");
+const burgerMenu = document.querySelector(".menu-burger");
+const menu = document.querySelector(".main-menubar");
+const navitem = document.querySelectorAll(".nav-item");
+
+document.addEventListener("DOMContentLoaded", () => {
+  menuBar.addEventListener("click", (e) => {
+    const mb = burgerMenu.classList.value;
+    if (!mb.includes("active")) {
+      burgerMenu.classList.add("active");
+      menu.classList.add("active");
+    } else {
+      burgerMenu.classList.remove("active");
+      menu.classList.remove("active");
+    }
+    e.preventDefault();
+  });
+  navitem.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      item.classList.toggle("active");
+      e.preventDefault();
+    });
+  });
+});
+// NavBar Sticky
+const navBar = document.querySelector(".navbar-div");
+
+document.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 10) {
+      navBar.classList.add("fixed");
+    } else {
+      navBar.classList.remove("fixed");
+    }
+  });
+});
+
 // Login After Navbar
 
-let itemTopbar = document.querySelectorAll('.loginNav')
-let loginAuthBtn =  document.querySelector('.loginAuth')
-let topBar = document.getElementById('topbar')
-let authBtn = document.querySelector('.auth-buttons')
-let customerAccountLogin = document.getElementById('customerAccountLogin')
+let itemTopbar = document.querySelectorAll(".loginNav");
+let loginAuthBtn = document.querySelector(".loginAuth");
+let topBar = document.getElementById("topbar");
+let authBtn = document.querySelector(".auth-buttons");
+let customerAccountLogin = document.getElementById("customerAccountLogin");
 
-let customerLogin =  false;
+let customerLogin = false;
 
-loginAuthBtn.addEventListener('click',(e)=>{
-    itemTopbar.forEach((item)=>{
-        console.log(item);
-        item.classList.remove('hidden')
-        
-    })
-    topBar.classList.add('bg-gradient-p')
-    topBar.classList.remove('bg-primary')
+loginAuthBtn.addEventListener("click", (e) => {
+  itemTopbar.forEach((item) => {
+    console.log(item);
+    item.classList.remove("hidden");
+  });
+  topBar.classList.add("bg-gradient-p");
+  topBar.classList.remove("bg-primary");
 
-    customerAccountLogin.classList.remove('hidden')
-    authBtn.classList.add('lg:hidden')
+  customerAccountLogin.classList.remove("hidden");
+  authBtn.classList.add("lg:hidden");
 
-    e.preventDefault()
-})
+  e.preventDefault();
+});
 
 // logo marquee
 document.addEventListener("DOMContentLoaded", () => {
-    const teamScroll = document.querySelector(".team-scroll");
-  
-    const cloneItems = () => {
-      const items = Array.from(teamScroll.children);
-      const totalWidth = items.reduce((acc, item) => acc + item.offsetWidth, 0);
-      const visibleWidth = teamScroll.parentElement.offsetWidth;
-  
-      if (totalWidth < visibleWidth * 2) {
-        items.forEach((item) => {
-          const clone = item.cloneNode(true);
-          teamScroll.appendChild(clone);
-        });
-        cloneItems(); 
-      }
-    };
-  
-    cloneItems();
-  
-    let scrollPosition = 0;
-    const scrollSpeed = 1; 
-  
-    const scrollMarquee = () => {
-      scrollPosition -= scrollSpeed;
-      if (Math.abs(scrollPosition) >= teamScroll.scrollWidth / 2) {
-        scrollPosition = 0;
-      }
-      teamScroll.style.transform = `translateX(${scrollPosition}px)`;
-      requestAnimationFrame(scrollMarquee);
-    };
-  
-    scrollMarquee();
-  });
-  
+  const teamScroll = document.querySelector(".team-scroll");
+
+  const cloneItems = () => {
+    const items = Array.from(teamScroll.children);
+    const totalWidth = items.reduce((acc, item) => acc + item.offsetWidth, 0);
+    const visibleWidth = teamScroll.parentElement.offsetWidth;
+
+    if (totalWidth < visibleWidth * 2) {
+      items.forEach((item) => {
+        const clone = item.cloneNode(true);
+        teamScroll.appendChild(clone);
+      });
+      cloneItems();
+    }
+  };
+
+  cloneItems();
+
+  let scrollPosition = 0;
+  const scrollSpeed = 1;
+
+  const scrollMarquee = () => {
+    scrollPosition -= scrollSpeed;
+    if (Math.abs(scrollPosition) >= teamScroll.scrollWidth / 2) {
+      scrollPosition = 0;
+    }
+    teamScroll.style.transform = `translateX(${scrollPosition}px)`;
+    requestAnimationFrame(scrollMarquee);
+  };
+
+  scrollMarquee();
+});
+// property Details Read more
+const readMore = document.querySelector('.read-more')
+
+readMore.addEventListener('click', function (e) {
+  e.preventDefault(); // Prevent the default anchor behavior
+  const moreText = document.querySelector('.more-text');
+  const readMoreLink = e.target;
+
+  if (moreText.style.display === 'none' || moreText.style.display === '') {
+    moreText.style.display = 'inline'; // Show the additional text
+    readMoreLink.textContent = 'Read less'; // Change link text
+  } else {
+    moreText.style.display = 'none'; // Hide the additional text
+    readMoreLink.textContent = 'Read more'; // Reset link text
+  }
+});
+
 //   Youtube Video JS
 
 function playVideo(thumbnail) {
-    const videoDiv = thumbnail.nextElementSibling;
-    const thumbnailDiv = thumbnail;
-  
-    // Show the iframe video and hide the thumbnail
-    videoDiv.style.display = "block";
-    thumbnailDiv.style.display = "none";
-  }
-  
+  const videoDiv = thumbnail.nextElementSibling;
+  const thumbnailDiv = thumbnail;
+
+  // Show the iframe video and hide the thumbnail
+  videoDiv.style.display = "block";
+  thumbnailDiv.style.display = "none";
+}
+
 // image zoom modal
 
 // Function to show the modal
 function showModal() {
-  const modal = document.getElementById('modal01');
-  modal.style.display = 'block';
+  const modal = document.getElementById("modal01");
+  modal.style.display = "block";
 }
 
 // Function to hide the modal
 function hideModal() {
-  const modal = document.getElementById('modal01');
-  modal.style.display = 'none';
+  const modal = document.getElementById("modal01");
+  modal.style.display = "none";
 }
 
-// Bid Dropdown 
+// Bid Dropdown
 document.addEventListener("DOMContentLoaded", () => {
-const bidBtn = document.querySelector('.bid-dropdown-btn');
-const bidDropdown = document.querySelector('.bid-dropdown');
+  const bidBtn = document.querySelector(".bid-dropdown-btn");
+  const bidDropdown = document.querySelector(".bid-dropdown");
 
-let bidDrop = false;
+  let bidDrop = false;
 
-bidBtn.addEventListener('click', () => {
-  bidDrop = !bidDrop; 
-  console.log(bidDrop);
-  
-  if (bidDrop) {
+  bidBtn.addEventListener("click", () => {
+    bidDrop = !bidDrop;
     console.log(bidDrop);
-    bidDropdown.classList.remove('hidden');
-    bidDropdown.classList.add('show');
-    bidBtn.classList.add('active')
-  } else {
-    console.log(bidDrop);
-    bidDropdown.classList.remove('show');
-    bidDropdown.classList.add('hidden');
-    bidBtn.classList.remove('active')
-  }
-});
+
+    if (bidDrop) {
+      console.log(bidDrop);
+      bidDropdown.classList.remove("hidden");
+      bidDropdown.classList.add("show");
+      bidBtn.classList.add("active");
+    } else {
+      console.log(bidDrop);
+      bidDropdown.classList.remove("show");
+      bidDropdown.classList.add("hidden");
+      bidBtn.classList.remove("active");
+    }
+  });
 });
 
 // Login Progress Js
 document.addEventListener("DOMContentLoaded", () => {
-const registerForm = document.querySelector('.r-form-a')
-const verificationCode = document.querySelector('.v-form')
-const ProgressBar = document.querySelectorAll('.progress-status')
-const documentUpload = document.querySelector('.document-form')
+  const registerForm = document.querySelector(".r-form-a");
+  const verificationCode = document.querySelector(".v-form");
+  const ProgressBar = document.querySelectorAll(".progress-status");
+  const documentUpload = document.querySelector(".document-form");
 
-const registerBtn = document.querySelector('#register-user')
-const verifyCodeBtn = document.getElementById('verifyCode-btn')
-let progressCount = 1
+  const registerBtn = document.querySelector("#register-user");
+  const verifyCodeBtn = document.getElementById("verifyCode-btn");
+  let progressCount = 1;
 
-registerBtn.addEventListener('click', (e,progressCount)=>{
-  registerForm.classList.add('hidden')
-  verificationCode.classList.remove('hidden')
-  console.log(e)
-  ProgressBar.forEach((item,e)=>{
-    if(item.id == 'step2'){
-      item.classList.add('active')
-    }
-    console.log(item,item.id)
-  })
- 
-})
-verifyCodeBtn.addEventListener('click',()=>{
-  verificationCode.classList.add('hidden');
-  documentUpload.classList.remove('hidden')
-  ProgressBar.forEach((item,e)=>{
-    if(item.id == 'step3'){
-      item.classList.add('active')
-    }
-    console.log(item,item.id)
-  })
-})
-});    
-
+  registerBtn.addEventListener("click", (e, progressCount) => {
+    registerForm.classList.add("hidden");
+    verificationCode.classList.remove("hidden");
+    console.log(e);
+    ProgressBar.forEach((item, e) => {
+      if (item.id == "step2") {
+        item.classList.add("active");
+      }
+      console.log(item, item.id);
+    });
+  });
+  verifyCodeBtn.addEventListener("click", () => {
+    verificationCode.classList.add("hidden");
+    documentUpload.classList.remove("hidden");
+    ProgressBar.forEach((item, e) => {
+      if (item.id == "step3") {
+        item.classList.add("active");
+      }
+      console.log(item, item.id);
+    });
+  });
+});
 
 // live auction lots animation
 document.addEventListener("DOMContentLoaded", () => {
   const tabs = document.querySelectorAll(".auction-tabs li");
   const glider = document.querySelector(".glider");
 
-  // Function to update the glider position and width
   const updateGlider = (activeTab) => {
     const tabWidth = activeTab.offsetWidth;
     const tabLeft = activeTab.offsetLeft;
 
-    // Set the glider width and position dynamically
     glider.style.width = `${tabWidth}px`;
     glider.style.transform = `translateX(${tabLeft}px)`;
   };
 
-  // Initial setup for the glider
   const initialActiveTab = document.querySelector(".auction-tabs li.active");
   if (initialActiveTab) updateGlider(initialActiveTab);
 
-  // Add event listeners to each tab
   tabs.forEach((tab) => {
     tab.addEventListener("click", () => {
-      // Remove active class from all tabs
       tabs.forEach((t) => t.classList.remove("active"));
 
-      // Add active class to the clicked tab
       tab.classList.add("active");
 
-      // Update the glider position and width
       updateGlider(tab);
     });
   });
 });
+
+// Selfi Photo FN
+const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
+const selfieImage = document.getElementById("selfie");
+const captureButton = document.getElementById("capture");
+const modalSelfi = document.querySelector(".modal-selfi");
+const selfi = document.querySelector(".selfi-btn");
+
+let captured = true;
+modalSelfi.addEventListener("click", () => {
+  modalSelfi.classList.add("hidden");
+});
+
+selfi.addEventListener("click", () => {
+  modalSelfi.classList.remove("hidden");
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then((stream) => {
+      video.srcObject = stream;
+    })
+    .catch((err) => {
+      console.error("Error accessing the camera: ", err);
+    });
+    
+});
+
+captureButton.addEventListener("click", () => {
+
+  selfieImage.classList.remove("hidden");
+  const context = canvas.getContext("2d");
+  context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+  const dataUrl = canvas.toDataURL("image/png");
+  selfieImage.src = dataUrl;
+
+  captured = false;
+  console.log(captured)
+
+  
+});
+
